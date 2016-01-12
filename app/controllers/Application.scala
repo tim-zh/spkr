1411b92e8243b8a7eb06f8f2350d45e8666b6644
tim-zh <tim.zhlobich@gmail.com>
2016-01-12 14:00:55 +0300

    load balancing; login, register stubs

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,6 +1,9 @@
 package controllers
 
 import play.api._
+import play.api.data._
+import play.api.data.Forms._
+import play.api.libs.json.Json
 import play.api.mvc._
 import reactivemongo.bson.BSONDocument
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -13,4 +16,31 @@ class Application extends Controller {
       Ok(list.size + "")
     }
   }
+
+  def login = Action { implicit request =>
+    Form(mapping("name" -> nonEmptyText, "pass" -> nonEmptyText)(Login.apply)(Login.unapply)).bindFromRequest.fold(
+      bad => {
+        Ok(Json.arr(bad.errors.map(err => Json.obj(err.key -> err.message))))
+      },
+      form => {
+        Ok(Json.obj("sid" -> 1))
+      }
+    )
+  }
+
+  def register = Action { implicit request =>
+    Form(mapping("name" -> nonEmptyText, "pass" -> nonEmptyText, "pass2" -> nonEmptyText)(Register.apply)(Register.unapply)).bindFromRequest.fold(
+      bad => {
+        Ok(Json.arr(bad.errors.map(err => Json.obj(err.key -> err.message))))
+      },
+      form => {
+        Ok(Json.obj("sid" -> 1))
+      }
+    )
+  }
+
+
+  private case class Login(name: String, pass: String)
+
+  private case class Register(name: String, pass: String, pass2: String)
 }
