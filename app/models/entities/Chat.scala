009cc3f116d4f5f530516d9fda1673f793e6403f
tim-zh <tim.zhlobich@gmail.com>
2016-02-02 19:55:25 +0300

    audio in separate collection

--- app/models/entities/Chat.scala
+++ app/models/entities/Chat.scala
@@ -7,7 +7,6 @@ import org.mongodb.morphia.annotations._
 case class Chat(var title: String,
                 @Reference
                 var history: java.util.List[Message],
-                @Reference(idOnly = true)
                 var users: java.util.List[ObjectId]) {
   @Id
   var id: ObjectId = _
